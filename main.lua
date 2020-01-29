5c4aff4a6a34b01e0bd9e0e85d71a45ae372926a
Kjeld Schmidt <contact@kjeld-schmidt.com>
2020-01-29 12:48:36 +0100

    Filtering nameplates to only include players

--- main.lua
+++ main.lua
@@ -4,11 +4,21 @@ SLASH_LOWSPEAK3 = "/qs"
 SLASH_LOWSPEAK4 = "/lowspeak"
 SLASH_LOWSPEAK5 = "/low"
 SLASH_LOWSPEAK6 = "/quiet"
-SlashCmdList["LOWSPEAK"] = function(msg)
-	for i = 1, 40 do
-		local name = UnitName("nameplate"..i)
-		if name then
-			print(i, name)
+SlashCmdList[ "LOWSPEAK" ] = function( msg )
+	local nameplate_shown = GetCVarBool( "nameplateShowFriends" )
+	if not nameplate_shown then
+		print("Friendly nameplates were not turned on - the message could not be delivered.")
+		print("The addon has turned them on automatically now. Please resend your message!")
+		SetCVar("nameplateShowFriends", 1)
+	else
+		for i = 1, 40 do
+			local unitID = "nameplate"..i
+			local name = UnitName( unitID )
+			if name then
+				if UnitIsPlayer( unitID ) and UnitIsFriend( "player", unitID ) then
+					print( name .. " will receive message" )
+				end
+			end
 		end
 	end
 end
